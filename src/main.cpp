10c2b4c13631a13126961f0c08180f1065f3a5ef
maxh144 <maxhug@u.northwestern.edu>
2026-02-14 16:35:02 -0600

    PID base
    Cut unnecessary content from the example, added PID controller with feed forward term to the callback

---
+++ src/main.cpp
@@ -0,0 +1,161 @@
+#include <Arduino.h>
+#include "ODriveCAN.h"
+#include <FlexCAN_T4.h>
+#include "ODriveFlexCAN.hpp"
+
+// CAN bus baudrate. Make sure this matches for every device on the bus
+#define CAN_BAUDRATE 25000
+// ODrive node_id for odrv0
+#define ODRV0_NODE_ID 0
+#define IS_TEENSY_BUILTIN // Teensy boards with built-in CAN interface (e.g. Teensy 4.1). See below to select which interface to use.
+
+
+struct ODriveUserData {
+  Heartbeat_msg_t last_heartbeat;
+  bool received_heartbeat = false;
+  Get_Encoder_Estimates_msg_t last_feedback;
+  bool received_feedback = false;
+};
+
+struct ODriveStatus; // hack to prevent teensy compile error
+FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can_intf;
+ODriveCAN odrv0(wrap_can_intf(can_intf), ODRV0_NODE_ID); // Standard CAN message ID
+ODriveCAN* odrives[] = {&odrv0}; // Make sure all ODriveCAN instances are accounted for here
+ODriveUserData odrv0_user_data; // Keep some application-specific user data for every ODrive.
+
+volatile float ref = 0;
+
+// void onCanMessage(const CanMsg& msg);  // forward declaration of function used
+
+bool setupCan() {
+  can_intf.begin();
+  can_intf.setBaudRate(CAN_BAUDRATE);
+  can_intf.setMaxMB(16);
+  can_intf.enableFIFO();
+  can_intf.enableFIFOInterrupt();
+  can_intf.onReceive(onCanMessage);
+  return true;
+}
+
+// Called every time a Heartbeat message arrives from the ODrive
+void onHeartbeat(Heartbeat_msg_t& msg, void* user_data) {
+  ODriveUserData* odrv_user_data = static_cast<ODriveUserData*>(user_data);
+  odrv_user_data->last_heartbeat = msg;
+  odrv_user_data->received_heartbeat = true;
+}
+
+// Called every time a feedback message arrives from the ODrive
+void onFeedback(Get_Encoder_Estimates_msg_t& msg, void* user_data) {
+  ODriveUserData* odrv_user_data = static_cast<ODriveUserData*>(user_data);
+  odrv_user_data->last_feedback = msg;
+  odrv_user_data->received_feedback = true;
+  runControl();
+}
+
+void runControl(){
+  Get_Encoder_Estimates_msg_t feedback = odrv0_user_data.last_feedback;
+  static long prev_time = 0;
+  static float e_prev = 0;
+  static float kp = 1.0f;
+  static float kd = 1.0f;
+  static float ff_gain = 1.0f;
+  static float angle_tare = 0.0f;
+
+  long curr_time = millis();
+  float dt = (curr_time-prev_time)/1000.0;
+
+  float e = ref - feedback.Pos_Estimate;
+  float ff = sinf(e-angle_tare);
+
+  float u = kp*e + kd*(e-e_prev)/dt + ff_gain*ff;
+
+  prev_time = curr_time;
+  e_prev = e;
+}
+
+// Called for every message that arrives on the CAN bus
+void onCanMessage(const CanMsg& msg) {
+  for (auto odrive: odrives) {
+    onReceive(msg, *odrive);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Wait for up to 3 seconds for the serial port to be opened on the PC side.
+  // If no PC connects, continue anyway.
+  for (int i = 0; i < 30 && !Serial; ++i) {
+    delay(100);
+  }
+  delay(200);
+  
+  Serial.println("Starting ODriveCAN demo");
+
+  // Register callbacks for the heartbeat and encoder feedback messages
+  odrv0.onFeedback(onFeedback, &odrv0_user_data);
+  odrv0.onStatus(onHeartbeat, &odrv0_user_data);
+
+  // Configure and initialize the CAN bus interface. This function depends on
+  // your hardware and the CAN stack that you're using.
+  if (!setupCan()) {
+    Serial.println("CAN failed to initialize: reset required");
+    while (true); // spin indefinitely
+  }
+  else{
+    Serial.println("CAN initialized");
+
+  }
+
+  Serial.println("Waiting for ODrive...");
+  while (!odrv0_user_data.received_heartbeat) {
+    pumpEvents(can_intf);
+  }
+
+  Serial.println("found ODrive");
+
+  // request bus voltage and current (1sec timeout)
+  Serial.println("attempting to read bus voltage and current");
+  Get_Bus_Voltage_Current_msg_t vbus;
+  if (!odrv0.request(vbus, 1000)) {
+    Serial.println("vbus request failed!");
+    while (true); // spin indefinitely
+  }
+
+  Serial.print("DC voltage [V]: ");
+  Serial.println(vbus.Bus_Voltage);
+  Serial.print("DC current [A]: ");
+  Serial.println(vbus.Bus_Current);
+
+  Serial.println("Enabling closed loop control...");
+  while (odrv0_user_data.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL) {
+    odrv0.clearErrors();
+    delay(1);
+    odrv0.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
+
+    for (int i = 0; i < 15; ++i) {
+      delay(10);
+      pumpEvents(can_intf);
+    }
+  }
+
+  Serial.println("ODrive running!");
+}
+
+void loop() {
+  float SINE_PERIOD = 2.0f; // Period of the position command sine wave in seconds
+  float t = 0.001 * millis();
+  float phase = t * (TWO_PI / SINE_PERIOD);
+  ref = sin(phase);
+
+  // print position and velocity for Serial Plotter
+  if (odrv0_user_data.received_feedback) {
+    Get_Encoder_Estimates_msg_t feedback = odrv0_user_data.last_feedback;
+    odrv0_user_data.received_feedback = false;
+    Serial.print("odrv0-pos:");
+    Serial.print(feedback.Pos_Estimate);
+    Serial.print(",");
+    Serial.print("odrv0-vel:");
+    Serial.println(feedback.Vel_Estimate);
+  }
+}
